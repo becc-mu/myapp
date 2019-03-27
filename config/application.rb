76552b6bc221420b5b62f77191d207234486a00c
becc-mu <becc.mulugeta@gmail.com>
2019-03-27 18:55:27 +0000

    added config setup

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,16 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.generators do |g|
+  g.test_framework :rspec,
+    :fixtures => true,
+    :view_specs => false,
+    :helper_specs => false,
+    :routing_specs => false,
+    :controller_specs => true,
+    :request_specs => true
+  g.fixture_replacement :factory_girl, :dir => "spec/factories"
+end
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
