414199b932625561366c1a4aa8dd22327f95e416
becc-mu <becc.mulugeta@gmail.com>
2019-03-28 12:56:32 +0000

    red undefined method title

---
+++ db/migrate/20190328125133_add_title_to_posts.rb
@@ -0,0 +1,4 @@
+class AddTitleToPosts < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ spec/models/blog_post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe BlogPost do
+
+  it 'is instantiable' do
+    expect{ blog_post = BlogPost.new }.not_to raise_error
+  end
+
+  it 'defaults fields to nil' do
+    blog_post = BlogPost.new
+
+    expect(blog_post.title).to be_nil
+    expect(blog_post.body).to be_nil
+  end
+end
