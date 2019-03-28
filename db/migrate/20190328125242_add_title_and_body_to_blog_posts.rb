d6251657af8e233837a1f8d47584c4b8d3a96e16
becc-mu <becc.mulugeta@gmail.com>
2019-03-28 13:01:03 +0000

    The action 'index' could not be found for BlogPostsController

--- db/migrate/20190328125242_add_title_and_body_to_blog_posts.rb
+++ db/migrate/20190328125242_add_title_and_body_to_blog_posts.rb
@@ -1,4 +1,6 @@
 class AddTitleAndBodyToBlogPosts < ActiveRecord::Migration[5.2]
   def change
+    add_column :blog_posts, :title, :string, null:, false
+    add_column :blog_posts, :body, :string, null:, false
   end
 end
